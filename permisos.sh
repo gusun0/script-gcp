7b5c431553228c324cdf1d8971851d29ba2e2955
Abraham <abr.alfaros@outlook.com>
2022-11-07 21:29:13 -0600

    permisos sh

---
+++ permisos.sh
@@ -0,0 +1,11 @@
+PROJECT_ID=$(gcloud config get-value project)
+PROJECT_NUMBER="$(gcloud projects describe ${PROJECT_ID} \
+	--format='get(projectNumber)¿)"
+cat >/tmp/kubernetes-cd-env-policy.yaml <<EOF
+bindings:
+ - members:
+   - serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com
+   role: roles.source.writer
+  EOF
+gcloud source repos set-iam-policy \
+	kubernetes-cd-env /tmp/kubernetes-cd-env-policy.yaml
